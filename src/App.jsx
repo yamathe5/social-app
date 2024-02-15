64e620fc965d53c68558e231fd81be01fbbdaeef
Yamathe5-laptop <manuelsegmar@gmail.com>
2024-02-15 12:23:29 -0500

    ADD: Auth provider & routes authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,70 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import PropTypes from "prop-types";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import FeedPage from "./pages/FeedPage"; // Asumiendo que tienes un componente FeedPage para el feed de tu red social
+import { useAuth } from "./contexts/auth";
 
 function App() {
+  const { auth } = useAuth();
+  console.log(auth);
+
+  if (!auth) {
+    console.log(!auth);
+    console.log({});
+    console.log(!{});
+    console.log("bien");
+  }
+
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/feed" element={<FeedPage />} />
-        {/* Añade una ruta para el index que redirija al feed o login dependiendo de si el usuario está autenticado */}
-        <Route path="/" element={<LoginPage />} />
+        <Route
+          path="/login"
+          element={<PublicRoute redirectTo="/feed" element={<LoginPage />} />}
+        />
+        <Route
+          path="/signup"
+          element={<PublicRoute redirectTo="/feed" element={<SignupPage />} />}
+        />
+        <Route
+          path="/feed"
+          element={
+            <ProtectedRoute redirectTo="/login" element={<FeedPage />} />
+          }
+        />
+        <Route path="/" element={<Navigate to="/feed" replace />} />
+        <Route path="*" element={<Navigate to="/feed" replace />} />
+
       </Routes>
     </Router>
   );
 }
+
+
+function PublicRoute({ redirectTo, element }) {
+  const { auth } = useAuth();
+  return !auth ? element : <Navigate to={redirectTo} replace />;
+}
+
+PublicRoute.propTypes = {
+  element: PropTypes.element.isRequired,
+  redirectTo: PropTypes.string.isRequired,
+};
+function ProtectedRoute({ redirectTo, element }) {
+  const { auth } = useAuth();
+  return auth ? element : <Navigate to={redirectTo} replace />;
+}
+
+ProtectedRoute.propTypes = {
+  element: PropTypes.element.isRequired,
+  redirectTo: PropTypes.string.isRequired,
+};
+
 
 export default App;
---
+++ src/contexts/auth.jsx
@@ -0,0 +1,145 @@
+// AuthContext.js
+import { jwtDecode  as jwt_decode } from 'jwt-decode';
+import { useState, createContext, useContext, useEffect } from "react";
+import PropTypes from "prop-types";
+
+// Create a context for auth
+const AuthContext = createContext(null);
+
+// Create a provider component
+export const AuthProvider = ({ children }) => {
+
+  const [auth, setAuth] = useState({
+    user: null, // Los detalles del usuario
+    token: localStorage.getItem('authToken') || null, // El token JWT
+  });
+
+
+  const fetchUserProfile = async (_id, token) => {
+    try {
+      const response = await fetch(`http://localhost:3000/api/user/${_id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const data = await response.json();
+      if (response.ok) {
+        setAuth((prevAuth) => ({ ...prevAuth, user: data }));
+      } else {
+        console.error("Error al obtener el perfil del usuario:", data.message);
+      }
+    } catch (error) {
+      console.error("Error en la petición al perfil del usuario:", error);
+    }
+  };
+
+
+
+  // Simulate a login function
+  const login = async (email, password) => {
+    console.log("entra")
+    try {
+      const response = await fetch(`http://localhost:3000/api/login`, {
+        method: "POST",
+        mode: "cors",
+        body: JSON.stringify({ email: email, password: password }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      console.log("sale")
+
+      console.log(response)
+      const data = await response.json();
+      console.log(data)
+      console.log("================")
+      // Asegúrate de que el inicio de sesión fue exitoso antes de actualizar el estado
+      if (response.ok) {
+        const { _id, token } = data;
+        setAuth({ token }); // Solo guardamos el token inicialmente
+        localStorage.setItem('authToken', token); // Guardamos el token en localStorage
+        fetchUserProfile(_id, token); // Obtenemos el perfil del usuario
+      } else {
+        console.error(data.message)
+        // Manejo de errores
+      }
+    } catch (error) {
+      console.error("Error al intentar iniciar sesión:", error);
+    }
+  };
+  
+  // Simulate a signup function
+const signup = async (body, type) => {
+  console.log(body, type)
+  try {
+    const response = await fetch(`http://localhost:3000/api/signup`, {
+      method: "POST",
+      mode: "cors",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+    console.log(response)
+    // Asegúrate de que el registro fue exitoso antes de actualizar el estado
+
+    if (response.ok) {
+      const { _id, token } = data;
+      setAuth({ token }); // Solo guardamos el token inicialmente
+      localStorage.setItem('authToken', token); // Guardamos el token en localStorage
+      fetchUserProfile(_id, token); // Obtenemos el perfil del usuario
+    } else {
+      console.error(data.message);
+
+      // Manejo de errores
+    }
+    
+  } catch (error) {
+    console.error("Error al intentar registrar:", error);
+  }
+};
+
+
+  // Simulate a logout function
+  const logout = () => {
+    setAuth({ user: null, token: null });
+    localStorage.removeItem('authToken'); // Eliminamos el token de localStorage
+  };
+
+
+  // Check if user is logged in when the provider mounts
+  useEffect(() => {
+    const storedToken = localStorage.getItem('authToken');
+    if (storedToken) {
+      setAuth((prevAuth) => ({ ...prevAuth, token: storedToken }));
+      // Aquí asumimos que el userId está almacenado en el token y lo decodificamos
+      // Esto es común con JWT, pero depende de cómo estés manejando los tokens
+      const decoded = jwt_decode(storedToken); // Necesitarías una función para decodificar el JWT
+      console.log(decoded)
+      fetchUserProfile(decoded.id, storedToken);
+    }
+  }, []);
+
+  // Store the user in localStorage when it changes
+
+  // The context value that will be supplied to any descendants of this component.
+  const value = {
+    auth,
+    login,
+    logout,
+    signup,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+// Create a custom hook to use the auth context
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired, // children debe ser un nodo React
+};
