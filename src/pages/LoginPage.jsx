c6cfc6455d08696e51654cee01578bbdd2090982
Yamathe5-laptop <manuelsegmar@gmail.com>
2024-02-15 18:17:31 -0500

    ADD: login methods in login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
 import "./LoginPage.css"; // Asegúrate de crear un archivo CSS para estilizar tu página de inicio de sesión
+import { useAuth } from "../contexts/auth";
 
 export default function LoginPage() {
+  const [inputs, setInput] = useState({mail:"", password:""})
+  const {login} = useAuth()
+
+  function handleSetInputs(e, type){
+    setInput((prevInput) => ({
+      ...prevInput,
+      [type]: e.target.value
+    }));
+  }
+
   // Función para manejar el inicio de sesión (a implementar)
   const handleLogin = (e) => {
     e.preventDefault();
+    login(inputs.mail, inputs.password)
     // Aquí iría la lógica para manejar el inicio de sesión
     console.log("Inicio de sesión");
   };
@@ -14,11 +27,11 @@ export default function LoginPage() {
         <h2 className="login-title">social app</h2>
         <div className="input-container">
           <label htmlFor="email">Mail</label>
-          <input type="email" id="email" required />
+          <input type="email" id="email" value={inputs.mail} onChange={e =>handleSetInputs(e, "mail")} required />
         </div>
         <div className="input-container">
           <label htmlFor="password">Contraseña</label>
-          <input type="password" id="password" required />
+          <input type="password" id="password" value={inputs.password} onChange={e =>handleSetInputs(e, "password")} required />
         </div>
         <button type="submit" className="login-button">
           Iniciar sesión
