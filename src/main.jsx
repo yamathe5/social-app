69bdbc01fd17f66cc790bad37c9736a5bd783d54
Yamathe5-laptop <manuelsegmar@gmail.com>
2024-02-20 11:47:22 -0500

    ADD: initial backend warm-up request on app startup

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,18 @@ import App from "./App.jsx";
 import "./index.css";
 import { AuthProvider } from "./contexts/auth.jsx";
 
+const warmUpBackend = async () => {
+  try {
+     await fetch(`${import.meta.env.VITE_API_URL}`);
+    console.log('Backend warmed up successfully');
+  } catch (error) {
+    console.error('Error warming up the backend:', error);
+  }
+};
+
+warmUpBackend();
+
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
