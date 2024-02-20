33692c5df298f576f812e882fbbe9074a435310e
Yamathe5-laptop <manuelsegmar@gmail.com>
2024-02-19 23:36:08 -0500

    ADD: post comments & fix problems after update

--- src/pages/MyPostsPage.jsx
+++ src/pages/MyPostsPage.jsx
@@ -1,16 +1,18 @@
 import { formatDistanceToNow } from "date-fns";
-import { es } from "date-fns/locale"; // Importa el locale que necesites, en este caso español
+import { es } from "date-fns/locale";
 
-import PropTypes from "prop-types"; // Importa PropTypes
+import PropTypes from "prop-types";
 import { useState } from "react";
-import "./MyPostPage.css"; // Asegúrate de tener este archivo CSS en tu proyecto
+import "./MyPostPage.css";
 import { useEffect } from "react";
 import Logo from "../assets/logo.jpg";
 import { jwtDecode as jwt_decode } from "jwt-decode";
-
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
 import { useAuth } from "../contexts/auth";
 import Sidebar from "../components/Sidebar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import FriendsSidebar from "../components/FriendsSidebar";
 export default function MyPostPage() {
   const [data, setData] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -19,25 +21,22 @@ export default function MyPostPage() {
   const [inputValue, setInputValue] = useState("");
 
   useEffect(() => {
-    const decoded = jwt_decode(auth.token); // Necesitarías una función para decodificar el JWT
-    console.log(decoded);
+    const decoded = jwt_decode(auth.token);
     fetch(`http://localhost:3000/api/user/${decoded.id}/posts`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${auth.token}`, // Incluye el token aquí
+        Authorization: `Bearer ${auth.token}`,
       },
     })
       .then((resp) => resp.json())
       .then((resp) => {
-        console.log(resp)
         setData(resp);
       })
       .catch((error) => console.error("Error al cargar los posts:", error));
   }, [auth.token]);
 
   function toogleLike(type, postId) {
-    console.log(type, postId)
 
     fetch(`http://localhost:3000/api/posts/${postId}/${type}`, {
       method: "PATCH",
@@ -50,22 +49,27 @@ export default function MyPostPage() {
       },
     })
       .then((res) => res.json())
-      .then((res) => {
-        console.log(res)
+      .then(() => {
         const updatedPosts = data.map((post) => {
           if (post._id === postId) {
-            return {
+            const updatedPost = {
               ...post,
               likes: !post.hasLiked
                 ? [...post.likes, auth.user._id]
                 : post.likes.filter((id) => id !== auth.user._id),
               hasLiked: !post.hasLiked,
-            }; // Asume que el servidor devuelve el post actualizado
+            };
+            if (selectedPost && selectedPost._id === postId) {
+              setSelectedPost(updatedPost);
+            }
+            return updatedPost;
           }
           return post;
         });
-        setData(updatedPosts); // Actualiza el estado con los posts actualizados
-      }).catch(err => console.log(err));
+
+        setData(updatedPosts);
+      })
+      .catch((err) => console.log(err));
   }
 
   const openModal = (post) => {
@@ -92,18 +96,20 @@ export default function MyPostPage() {
         </nav>
       </header>
       <main className="feed__main">
-     
-                <Sidebar></Sidebar>
+        <Sidebar></Sidebar>
 
         <section className="feed__posts">
           {isModalOpen && (
             <Modal
-            inputValue={inputValue}
-            setInputValue={setInputValue}
-            post={selectedPost}
-            auth={auth}
-            setSelectedPost={setSelectedPost}
-            onClose={() => setIsModalOpen(false)}
+              data={data}
+              setData={setData}
+              toogleLike={toogleLike}
+              inputValue={inputValue}
+              setInputValue={setInputValue}
+              post={selectedPost}
+              auth={auth}
+              setSelectedPost={setSelectedPost}
+              onClose={() => setIsModalOpen(false)}
             />
           )}
 
@@ -111,11 +117,19 @@ export default function MyPostPage() {
             data.map((item, index) => (
               <article className="post" key={index}>
                 <h2 className="post__username">{item.user.username}</h2>
+
                 <p className="post__content">{item.content}</p>
-                <img className="post__image" src={item.image} alt="" />
-                <p>Comments {item.comments.length}</p>
+                <img className="post__image" src={item.signedImageUrl} alt="" />
+                <div className="post__stadistics">
+                  <span className="post__comment-quantity">
+                    Likes {item.likes.length}{" "}
+                  </span>
+                  <span className="post__comment-quantity">
+                    Comments {item.comments.length}{" "}
+                  </span>
+                </div>
                 <div className="post__buttons">
-                {item.hasLiked ? (
+                  {item.hasLiked ? (
                     <button
                       className="post__button post__button--liked"
                       onClick={() => toogleLike("unlike", item._id)}
@@ -140,26 +154,22 @@ export default function MyPostPage() {
               </article>
             ))}
         </section>
-        <aside className="feed__friends">
-          <h3 className="feed__friends-title">Friends</h3>
-          <ul className="feed__friends-list">
-            <li className="feed__friends-item">Camilo Rodríguez</li>
-            {/* Repite el elemento de lista para más amigos */}
-          </ul>
-        </aside>
+        <FriendsSidebar></FriendsSidebar>
       </main>
     </div>
   );
 }
 
-
 const Modal = ({
+  setData,
+  data,
   post,
   onClose,
   auth,
   setSelectedPost,
   inputValue,
   setInputValue,
+  toogleLike,
 }) => {
   if (!post) return null;
 
@@ -168,6 +178,8 @@ const Modal = ({
   }
 
   function onSubmit() {
+    if (inputValue.length === 0) return;
+
     fetch(`http://localhost:3000/api/posts/${post._id}/comments`, {
       method: "POST",
       mode: "cors",
@@ -181,63 +193,105 @@ const Modal = ({
     })
       .then((res) => res.json())
       .then((newComment) => {
-        // Actualiza el estado para incluir el nuevo comentario
-        // Asume que newComment es el comentario recién creado, devuelto por el servidor
-        setSelectedPost((prevPost) => ({
-          ...prevPost,
-          comments: [...prevPost.comments, newComment],
-        }));
-        setInputValue(""); // Limpia el campo de entrada
+        const updatedSelectedPost = {
+          ...post,
+          comments: [...post.comments, newComment],
+        };
+        setSelectedPost(updatedSelectedPost);
+
+        // Actualiza la lista global de posts
+        const updatedPosts = data.map((item) => {
+          if (item._id === post._id) {
+            return updatedSelectedPost;
+          }
+          return item;
+        });
+        setData(updatedPosts);
+
+        setInputValue("");
       })
       .catch((error) =>
         console.error("Error al agregar el comentario:", error)
       );
   }
+
   return (
     <div className="modal-backdrop">
       <div className="modal">
         <article className="modal_post">
-          <h2 className="post__username">{post.user.username}</h2>
+          <div className="modal-header">
+            <h2 className="post__username">{post.user.username}</h2>
+            <FontAwesomeIcon
+              icon={faTimes}
+              className="modal-close-icon"
+              onClick={onClose}
+            />
+          </div>{" "}
           <p className="post__content">{post.content}</p>
-          {post.image && (
-            <img className="post__image" src={post.image} alt="Post" />
+          {post.signedImageUrl && (
+            <img className="post__image" src={post.signedImageUrl} alt="Post" />
           )}
-          <p>Comments {post.comments ? post.comments.length : 0}</p>
-          <div className="comments__container">
-          {post.comments &&
-            post.comments.map((comment, index) => (
-              <div key={index} className="comment">
-                <strong className="comment__username" >{comment.user.username}: </strong>
-                <br />
-                <span className="comment__content">{comment.text}</span>
-                <br />
-                <span className="comment__date">
-                  {formatDistanceToNow(new Date(comment.createdAt), {
-                    addSuffix: true,
-                    locale: es,
-                  })}
-                </span>
-              </div>
-            ))}
-
+          <div className="post__stadistics">
+            <span className="post__comment-quantity">
+              Likes {post.likes.length}{" "}
+            </span>
+            <span className="post__comment-quantity">
+              Comments {post.comments.length}{" "}
+            </span>
           </div>
-          
+          <div className="comments__container">
+            {post.comments &&
+              post.comments.map((comment, index) => (
+                <div key={index} className="comment">
+                  <strong className="comment__username">
+                    {comment.user.username}:{" "}
+                  </strong>
+                  <br />
+                  <span className="comment__content">{comment.text}</span>
+                  <br />
+                  <span className="comment__date">
+                    {formatDistanceToNow(new Date(comment.createdAt), {
+                      addSuffix: true,
+                      locale: es,
+                    })}
+                  </span>
+                </div>
+              ))}
+          </div>
           <div className="post__buttons">
-            <button className="post__button">Me gusta</button>
+            {post.hasLiked ? (
+              <button
+                className="post__button post__button--liked"
+                onClick={() => toogleLike("unlike", post._id)}
+              >
+                Me gusta
+              </button>
+            ) : (
+              <button
+                className="post__button"
+                onClick={() => toogleLike("like", post._id)}
+              >
+                Me gusta
+              </button>
+            )}{" "}
             <button className="post__button" onClick={onClose}>
               Cerrar
             </button>
           </div>
-
-          <div className="input__box" >
+          <div className="input__box">
             <input
-            className="input__box__comment"
+              className="input__box__comment"
               type="text"
               value={inputValue}
               placeholder="Escribe un comentario"
               onChange={(e) => handleChangeInput(e)}
             />
-            <button className="input__box__send-button" onClick={() => onSubmit()}>Envia</button>
+            <button
+              className="input__box__send-button"
+              onClick={() => onSubmit()}
+            >
+              Envia
+            </button>
           </div>
         </article>
       </div>
@@ -248,11 +302,13 @@ Modal.propTypes = {
   post: PropTypes.shape({
     _id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
+    hasLiked: PropTypes.bool.isRequired,
+
     content: PropTypes.string.isRequired,
     image: PropTypes.string,
+    signedImageUrl: PropTypes.string,
     user: PropTypes.shape({
-      username: PropTypes.string.isRequired, // Asume que user es un objeto con un campo username
-      // Puedes añadir más campos del usuario aquí si es necesario
+      username: PropTypes.string.isRequired,
     }).isRequired,
     likes: PropTypes.arrayOf(PropTypes.string),
     comments: PropTypes.arrayOf(
@@ -261,9 +317,7 @@ Modal.propTypes = {
         text: PropTypes.string.isRequired,
         user: PropTypes.shape({
           username: PropTypes.string.isRequired,
-          // Incluye otros campos del modelo de usuario si son necesarios aquí
         }).isRequired,
-        // Añade aquí más campos según tu modelo
       })
     ),
     createdAt: PropTypes.string.isRequired,
@@ -273,11 +327,37 @@ Modal.propTypes = {
     token: PropTypes.string.isRequired,
     user: PropTypes.shape({
       _id: PropTypes.string.isRequired,
-      // Incluye otros campos del modelo de usuario si son necesarios aquí
     }),
   }),
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      title: PropTypes.string,
+      content: PropTypes.string.isRequired,
+      hasLiked: PropTypes.bool.isRequired,
+      image: PropTypes.string,
+      user: PropTypes.shape({
+        username: PropTypes.string.isRequired,
+      }),
+      likes: PropTypes.arrayOf(PropTypes.string),
+      comments: PropTypes.arrayOf(
+        PropTypes.shape({
+          _id: PropTypes.string.isRequired,
+          text: PropTypes.string.isRequired,
+          user: PropTypes.shape({
+            username: PropTypes.string.isRequired,
+          }),
+        })
+      ),
+      createdAt: PropTypes.string.isRequired,
+      updatedAt: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  setData: PropTypes.func.isRequired,
+
   onClose: PropTypes.func.isRequired,
-  setSelectedPost: PropTypes.func.isRequired, // Añade esta línea
+  setSelectedPost: PropTypes.func.isRequired,
   inputValue: PropTypes.string.isRequired,
   setInputValue: PropTypes.func.isRequired,
+  toogleLike: PropTypes.func.isRequired,
 };
