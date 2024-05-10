5d09fc393db63fd75206d866f1fd3225ea5ce546
Yamathe5-laptop <manuelsegmar@gmail.com>
2024-05-10 12:08:32 -0500

    ADD: Backend warming up function

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,11 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./LoginPage.css";
 import { useAuth } from "../contexts/auth";
 import { Link } from "react-router-dom";
 
 export default function LoginPage() {
-  const [inputs, setInput] = useState({ mail: "", password: "" });
+  const [inputs, setInput] = useState({ mail: "manuelsegmar@gmail.com", password: "password" });
+  const [backendReady, setBackendReady] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [statusMessage, setStatusMessage] = useState("Social App backend is loading...");
+
   const { login } = useAuth();
+  const apiUrl = import.meta.env.VITE_API_URL;
+  useEffect(() => {
+    const checkBackend = async () => {
+      try {
+        const response = await fetch(`${apiUrl}/api/users`);  //aqui cambiar con un endpoint especifico para retornar un 200
+
+        if (response.ok) {
+          setBackendReady(true);
+          setStatusMessage("Social App is Ready to use");
+        } else {
+          setStatusMessage("Could not connect to the backend");
+        }
+      } catch (error) {
+        setStatusMessage("Could not connect to the backend");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    checkBackend();
+  }, []);
 
   function handleSetInputs(e, type) {
     setInput((prevInput) => ({
@@ -14,14 +39,28 @@ export default function LoginPage() {
     }));
   }
 
-  const handleLogin = (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
-    login(inputs.mail, inputs.password);
-    console.log("Inicio de sesión");
+    setLoading(true);
+    setStatusMessage("Social App Backend is loading...");
+
+    try {
+      await login(inputs.mail, inputs.password);
+      setStatusMessage("Social App Ready to use");
+    } catch (error) {
+      setStatusMessage("Error: Could not connect to the backend");
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
     <div className="login-container">
+      <div className="login-status">
+        
+        {statusMessage}
+      
+      </div>
       <form className="login-form" onSubmit={handleLogin}>
         <h2 className="login-title">social app</h2>
         <div className="input-container">
@@ -44,8 +83,8 @@ export default function LoginPage() {
             required
           />
         </div>
-        <button type="submit" className="login-button">
-          Iniciar sesión
+        <button type="submit" className="login-button" disabled={!backendReady || loading}>
+          {loading ? "Loading..." : "Iniciar sesión"}
         </button>
         <div className="login-links">
           <Link to="/signup">No tienes una cuenta? Regístrate</Link>
